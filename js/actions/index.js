c75a6a635d76586db023826a6315b208b7a95847
dustwise <jared.mohney@gmail.com>
2017-03-13 09:40:50 -0600

    psuedocode for actions

---
+++ js/actions/index.js
@@ -0,0 +1,8 @@
+//actions needed
+//generate a new random number and hot and cold deviations from that number 
+//(ex: num 40, hot is 30-50, cold is outside of that range)
+//user inputs guess and check to see if correct
+//----save user guess
+//----check to see if guess is correct
+//--------if correct, prompt user
+//--------if incorrect, add number to guess list and prompt for new guess
